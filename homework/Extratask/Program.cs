b531fb73cc44914a05d17e7ea3ee525f73bc41e7
Guwel <guwel@yandex.ru>
2022-11-28 12:19:38 +0500

    homework

---
+++ homework/Extratask/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int premax = 0;
+while (n != 0)
+{
+    if (n>max)
+{
+    premax = max;
+    max = n;
+}
+else
+{
+    if (n>premax)
+    premax = n;
+}
+n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("end");
+Console.Write($"Второе максимальное число: {premax}");
+
+
+
+
+
+
+
+               
